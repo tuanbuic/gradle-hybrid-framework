f870213718e3931a8e87d23645e01117765427eb
[b13-kevinbui] <tuanbui@b13technology.com>
2022-12-30 18:00:47 +0700

    update cucumber setup

--- src/test/java/stepdef/facebookStep.java
+++ src/test/java/stepdef/facebookStep.java
@@ -1,5 +1,32 @@
 package stepdef;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class facebookStep {
+    WebDriver webdriver;
+    @Given("Open google chrome")
+    public void openGoogleChrome() {
+        webdriver = WebDriverManager.chromedriver().create();
+        webdriver.get("https://google.com");
+        webdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @When("I go to facebook")
+    public void iGoToFacebook() {
+        webdriver.get("https://facebook.com");
+    }
+
+    @Then("facebook worked")
+    public void facebookWorked() {
+        Assert.assertTrue(webdriver.findElement(By.id("email")).isDisplayed());
+    }
 
 }
